ad4b99d16621e95ad4c28b4170612384df3e2344
Robert N. Cahn <rncahn@lbl.gov>
2016-09-07 15:32:41 -0700

    debugging summer in paris

--- py/desisim/quicksurvey.py
+++ py/desisim/quicksurvey.py
@@ -263,8 +263,12 @@ class SimSetup(object):
         elg=((mtl['DESI_TARGET'] & desi_mask.ELG) != 0)
         more=(mtl['NUMOBS_MORE']>0)
         print ("ELGs in mtl needing observation %d"%len(mtl[elg & more]))
-        p = subprocess.call([self.fiberassign_exec, os.path.join(self.tmp_output_path, 'fa_features.txt')], stdout=subprocess.PIPE)
-
+        p = subprocess.Popen([self.fiberassign_exec, os.path.join(self.tmp_output_path, 'fa_features.txt')], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        for line in p.stdout:
+            #the real code does filtering here
+            print("stdout:",line.rstrip())
+        for line in p.stderr:
+            print("stderr:",line.rstrip())
         print("{} Finished fiberassign".format(asctime()))
         
 
